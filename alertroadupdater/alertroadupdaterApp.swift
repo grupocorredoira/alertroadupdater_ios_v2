a062d29587253de616c274eca8d33de2684f8ee8
vanesefa <insvsf00@gmail.com>
2025-03-17 11:24:47 +0100

    Initial Commit

---
+++ alertroadupdater/alertroadupdaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  alertroadupdaterApp.swift
+//  alertroadupdater
+//
+//  Created by Corredoira on 17/3/25.
+//
+
+import SwiftUI
+
+@main
+struct alertroadupdaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ alertroadupdaterTests/alertroadupdaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  alertroadupdaterTests.swift
+//  alertroadupdaterTests
+//
+//  Created by Corredoira on 17/3/25.
+//
+
+import Testing
+@testable import alertroadupdater
+
+struct alertroadupdaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
